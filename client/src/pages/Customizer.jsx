2ff43caf63c5a5cbcbbe140a3143ec1d921b68af
imshreyanshshukla <shreyanshshukla2004@gmail.com>
2023-10-02 20:06:23 +0530

    Added AI

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -11,6 +11,63 @@ import {AIPicker, ColorPicker, CustomButton, FilePicker, Tab} from '../component
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+  const [file, setFile] = useState('');
+  const [prompt, setprompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("")
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  })
+  const generateTabContent = () => {
+    switch(activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />
+      case "filepicker":
+        return <FilePicker 
+          file = {file}
+          setFile = {setFile}
+          readFile = {readFile}
+        />
+      case "aipicker":
+        return <AIPicker />
+      default:
+        return null;
+    }
+  }
+
+  const handleDecals = (type,result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+
+  const handleActiveFilterTab = (tabName) => {
+    switch(tabName){
+      case "logoShirt":
+        state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case "stylishShirt":
+        state.isFullTexture = !activeFilterTab[tabName];
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+    }
+  }
+
+  const readFile = (type) => {
+    reader(file)
+      .then((result) => {
+        handleDecals(type,result);
+        setActiveEditorTab("");
+      })
+  }
+
   return (
     <AnimatePresence>
       {!snap.intro &&(
@@ -26,9 +83,11 @@ const Customizer = () => {
                 <Tab 
                   key={tab.name}
                   tab={tab}
-                  handleClick={()=>{}}
+                  handleClick={()=> setActiveEditorTab(tab.name)}
                 />
               )}
+
+              {generateTabContent()}
             </div>
           </div>
         </motion.div>
